34e7cfebf5beb3bbafd2c937cccae64e67a5f0f1
Rahul Yadav <rahul2699271@gmail.com>
2020-07-03 21:13:18 +0530

    component selector

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  selector: 'app-servers', // as a html element
+  // selector: '[app-servers]', // as an attribute
+  // selector: '.app-servers', // as a class
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
