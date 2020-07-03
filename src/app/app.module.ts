0c845959f3728c099aba81aeca3e33e802d592ae
Rahul Yadav <rahul2699271@gmail.com>
2020-07-03 21:28:14 +0530

    practive assignment_1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { FormsModule } from "@angular/forms";
 import { AppComponent } from './app.component';
 import { ServerComponent } from "./server/server.component";
 import { ServersComponent } from './servers/servers.component';
+import { WarningComponent } from './warning/warning.component';
+import { SuccessAlertComponent } from './success-alert/success-alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ServerComponent,
-    ServersComponent
+    ServersComponent,
+    WarningComponent,
+    SuccessAlertComponent,
   ],
   imports: [
     BrowserModule
---
+++ src/app/warning/warning.component.ts
@@ -0,0 +1,9 @@
+import  { Component } from '@angular/core';
+
+@Component({
+    selector: 'r-warning',
+    templateUrl: './warning.component.html'
+})
+export class WarningComponent {
+
+}
